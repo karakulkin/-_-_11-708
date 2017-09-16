c9ac750d1668a522a38cb800967c0c7222b64e1c
karakulkin <yaprova@list.ru>
2017-09-16 14:25:04 +0800

    first commit

---
+++ Program2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 4;
+            int c = 0;
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
