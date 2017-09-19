8b4f0a15daff57dfffd7ff19f7123fb21bf63c7f
karakulkin <yaprova@list.ru>
2017-09-19 20:55:39 +0800

    first commit

---
+++ SEM/KOL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)//задачи на семинар expr 4
+        {
+            int k = 0;
+            int N = Convert.ToInt32(Console.ReadLine());
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < N; i++)
+            {
+                if (i % x == 0)
+                    k=k+1;
+                else
+                    if (i % y == 0)
+                    k=k+1;
+
+            }
+            Console.WriteLine(k);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SEM/hour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = Convert.ToInt32(Console.ReadLine());
+            if (h < 7)
+                h = h * 30;
+            else
+                h = 360 - (h * 30);
+            Console.WriteLine(h);
+            Console.ReadKey();
+
+        }
+    }
+}
